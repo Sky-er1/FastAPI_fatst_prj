089702de4c1caebeed270b53d78d739638fa17c1
sky-er <sky-er@yandex.ru>
2024-08-30 13:48:51 +0300

    change main

--- main.py
+++ main.py
@@ -13,34 +13,3 @@ app.include_router(router_users)
 app.include_router(router_bookings)
 
 
-class HotelsSearArgs:
-    def __init__(self, hotel_id: int,
-                 date_from: date,
-                 date_to: Optional[date] = None,
-                 stars: Optional[int] = Query(None, ge=1, le=5)
-                 ):
-        self.hotel_id = hotel_id
-        self.date_from = date_from
-        self.date_to = date_to
-        self.stars = stars
-
-
-class SHotel(BaseModel):
-    adress: str
-    name: str
-    stars: int
-
-
-list[SHotel]
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/hotels/{hotel_id}")
-async def hotels(search_args: HotelsSearArgs = Depends()):
-
-    return search_args
-
